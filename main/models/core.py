a3602af89a0e618b769c8e71afaf04f87f5ece94
Shamil Gumerov <shamil.gumerov@gmail.com>
2021-04-20 01:47:31 +0300

    chore[model]: Refactoring types and autodoc comments

--- main/models/core.py
+++ main/models/core.py
@@ -13,28 +13,27 @@ class Common(models.Model):
         abstract = True
 
 
-# Компания которой пренадлежат площадки
 class Company(Common):
+    """
+    Компания которой пренадлежит площадка
+    """
     short_name = models.CharField(help_text='Короткое наименование',max_length=100)
     full_name = models.CharField(help_text='Полное наименование', max_length=250)
     code = models.PositiveBigIntegerField(help_text='Код ЦФО', blank=False, default=0)
     root_dir = models.CharField(help_text='Каталог со статикой', max_length=100)
 
 
-# Типы площадок
-# Пока нет понимания что за типы.
-# Добавлено на развитие
-class AreaType(Common):
-    pass
-
-    def __str__(self):
-        return (self.name)
-
-
-# Площадки (Коворкинги)
 class Area(Common):
-    type = models.ForeignKey(AreaType, help_text= 'Тип площадки',
-                                on_delete=models.deletion.CASCADE, blank=True, null=True, default=None)
+    """
+    Площадки (Коворкинги)
+    """
+    AREA_TYPE_COWORKING = 'cow'
+    AREA_TYPE_FLOOR = 'flr'
+    AREA_TYPE = (
+        (AREA_TYPE_COWORKING, 'Коворкинг'),
+        (AREA_TYPE_FLOOR, 'Этаж'),
+    )
+    type = models.CharField(help_text='Тип площадки', max_length=3, choices=AREA_TYPE, default=AREA_TYPE_COWORKING)
     map_url = models.CharField(help_text='Карта площадки', max_length=100)
     company = models.ForeignKey(Company, help_text= 'Компания',
                                 on_delete=models.deletion.CASCADE, blank=True, null=True, default=None)
@@ -43,61 +42,82 @@ class Area(Common):
         return ("%s::%s (id: %s);"  % (self.type, self.name, self.id))
 
 
-# Дополнительная информация о пользователе
 class ExtUser(User):
+    """
+    Дополнительная информация о пользователе
+    """
     middle_name = models.CharField(help_text='Отчество', max_length=500)
     company = models.ForeignKey(Company, help_text='Компания', on_delete=models.deletion.CASCADE, blank=True, null=True, default=None)
     active = models.BooleanField(blank=True, default=True)
     def_area = models.ForeignKey(Area, help_text='Площадка по умолчанию', on_delete=models.deletion.CASCADE, blank=True, null=True, default=None)
 
 
-# Массовые сообщения для пользователей
 class Notification(Common):
+    """
+    Массовые сообщения для пользователей
+    """
     message = models.CharField(help_text='Сообщение', max_length=500)
     start = models.DateTimeField(help_text='Начало брони', blank=True)
     finish = models.DateTimeField(help_text='Окончание брони', blank=True)
     users_group = models.ForeignKey(Group, help_text= 'Группа', on_delete=models.deletion.CASCADE, blank=True, null=True, default=None)
     active = models.BooleanField(blank=True, default=True)
 
-# Ресурсы
+
 class Resource(Common):
-    area = models.ForeignKey(Area, help_text='Родительская площадка',
-                                  on_delete=models.deletion.CASCADE, blank=True, null=True, default=None)
+    """
+    Ресурсы
+    """
+    RESOURCE_TYPE_ALL = 'resource'
+    RESOURCE_TYPE_SEAT = 'seat'
+    RESOURCE_TYPE_ROOM = 'room'
+    area = models.ForeignKey(Area, help_text='Родительская площадка', on_delete=models.deletion.CASCADE, blank=True, null=True, default=None)
 
     @property
     def type(self):
+        """ Возвращает тип ресурса """
         if hasattr(self, 'room'):
-            return 'room'
+            return self.RESOURCE_TYPE_ROOM
         if hasattr(self, 'seat'):
-            return 'seat'
-        return 'resource'
+            return self.RESOURCE_TYPE_SEAT
+        return self.RESOURCE_TYPE_ALL
 
     def __str__(self):
         return ("Id: %s; Type: %s; Name: %s;" % (self.id, type, self.name))
 
-# Переговорные
+
 class Room(Resource):
+    """
+    Переговорные
+    """
     capacity = models.SmallIntegerField(help_text= 'Количество сидячих мест', default=0)
 
-# Рабочие места
+
 class Seat(Resource):
+    """
+    Рабочие места
+    """
     persisted = models.BooleanField(help_text= 'Постоянное место', blank=True, default=False)
     owner = models.ForeignKey(User, help_text= 'За кем закреплено', on_delete=models.deletion.CASCADE, blank=True, null=True, default=None)
 
-# Событие всязанное с бронированием
-# Например: Ежедневная планерка
-# Актуально только для переговорок (пока)
+
 class Event(Common):
+    """
+    Событие всязанное с бронированием
+    Например: Ежедневная планерка
+    Актуально только для переговорок (пока)
+    """
     description = models.CharField(help_text='Описание', max_length=500)
     users = models.ManyToManyField(User, blank=True, help_text= 'Участники', default=None)
 
 
-# Запросы пользователей к администрации коворкинга
-#  resource - Ресурс к которому привязан запрос. Пока предполагается что в основном это будет забронированное место.
-#  created_by - ползователь сгенерировавший запрос
-#  message - тело запроса
-#  active - активный или отработанный запрос
 class Request(Common):
+    """
+    Запросы пользователей к администрации коворкинга
+     resource - Ресурс к которому привязан запрос. Пока предполагается что в основном это будет забронированное место.
+     created_by - ползователь сгенерировавший запрос
+     message - тело запроса
+     active - активный или отработанный запрос
+    """
     created_at = models.DateTimeField(help_text='Время создания', auto_now_add=True)
     created_by = models.CharField(help_text= 'Кем создано', max_length=50, null=False, default='')
     resource = models.ForeignKey(Resource, help_text='Объект запроса', on_delete=models.deletion.CASCADE, blank=True, null=True, default=None)
@@ -105,13 +125,10 @@ class Request(Common):
     active = models.BooleanField(blank=True, default=True)
 
 
-# Записи о бронировании ресурсов
-#  confirmed         - подтверждение бронирования
-#  confirmed_at      - когда подтверждено
-#  confirmed_by      - кем подтверждено
-#  confirmation_pin  - пин-код который надо ввести для подтверждения
-# todo: уирать бланк=тру на пользователях и начале окончании бронирования
 class Booking(Common):
+    """
+    Записи о бронировании ресурсов
+    """
     created_at = models.DateTimeField(help_text='Время создания', auto_now_add=True)
     created_by = models.CharField(help_text= 'Кем создано', max_length=50, null=False, default='')
     changed_at = models.DateTimeField(help_text='Время изменения', auto_now=True)
