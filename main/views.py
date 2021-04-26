affbc6729b3d7000f580957ab8ba1fcadb69f882
Shamil Gumerov <shamil.gumerov@gmail.com>
2021-04-26 11:41:00 +0300

    feat[main]: Booking page and view

--- main/views.py
+++ main/views.py
@@ -11,8 +11,13 @@ def index(request):
 
 
 @login_required
-def home(request):
+def home(request, area_id=None):
     xuser = request.user.extuser
+
+    if area_id:
+        xuser.def_area_id = area_id
+        xuser.save()
+        return HttpResponseRedirect(reverse('main:home'))
 
     if not xuser.def_area:
         return HttpResponseRedirect(reverse('main:area_select'))
@@ -37,14 +42,22 @@ def home(request):
     return render(request, 'main/home.html', context)
 
 @login_required
-def booking(request, resource_id):
+def booking(request, resource_id, target_date=None):
     xuser = request.user.extuser
     resource = Resource.objects.get(pk=resource_id)
+    percent_of_booked_time = resource.get_percent_of_booked_time(target_date)
+
+    calendar = []
+    for c in resource.get_calendar(target_date):
+        calendar.append(c)
 
     context = {
         'user': request.user.extuser,
         'area': xuser.def_area,
         'resource': resource,
+        'calendar': calendar,
+        'target_date': target_date,
+        'percent_of_booked_time': percent_of_booked_time
     }
     return render(request, 'main/booking.html', context)
 
